2d7acd9b8bf2427ecd53a00a342f17a0a4ef5a5a
NemanjaKusic <nemanja.kusic99@gmail.com>
2021-12-13 13:11:25 +0100

    Dodate prve dve write komande i stred_read

--- stred.c
+++ stred.c
@@ -1,10 +1,20 @@
+#include <linux/uaccess.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/errno.h>
+#include <linux/kdev_t.h>
+#include <linux/device.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/types.h>
 #include <linux/cdev.h>
+#define STRING_SIZE 101
 
 MODULE_LICENSE("Dual BSD/GPL");
+
+char string[STRING_SIZE];
+int endRead = 0;
 
 dev_t my_dev_id;
 static struct class *my_class;
@@ -42,13 +52,78 @@ int stred_close(struct inode *pinode, struct file *pfile)
 
 ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
 {
-	printk(KERN_INFO "Succesfully read from file\n");
-	return 0;
+	int ret;
+	char buff[STRING_SIZE];
+	long int len;
+	
+	if (endRead){
+		endRead = 0;
+		printk(KERN_INFO "Succesfully read from file\n");
+		return 0;
+	}
+	
+	len = scnprintf(buff, STRING_SIZE, "String u buferu: %s\n" , string);
+	ret = copy_to_user(buffer, buff, len);
+	if(ret)
+		return -EFAULT;
+
+	endRead = 1;
+
+	return len;
 }
 
 ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
 {
-	printk(KERN_INFO "Succesfully wrote into file\n");
+	char buff[STRING_SIZE];
+	int ret;
+
+	ret = copy_from_user(buff, buffer, length);
+	if(ret)
+		return -EFAULT;
+
+	buff[length-1] = '\0';
+
+	if (strstr(buff, "string=") == buff)
+	{
+		char* reci = buff + 7; //da ne bi upisivao i string=
+		int i;
+		int len;
+
+		for (i=0; i<101; i++)
+		{
+			string[i] = 0;
+		}
+		
+		len = strlen(reci);
+		
+		if(len < 101)
+		{
+			strncpy(string, reci, len);
+			printk(KERN_INFO "Upisan string u bafer: %s\n", string);
+		}
+		else
+		{	
+			printk(KERN_ERR "Preko 100 karaktera ne moze se uneti");
+		}
+
+	}
+	else if(strstr(buff, "clear") == buff)
+	{
+		int i;
+
+		for (i=0; i<101; i++)
+		{
+			string[i] = 0;
+		}
+
+		printk(KERN_INFO "Uspesno obrisan string iz bufera");
+
+	}
+	else
+       	{
+		printk(KERN_WARNING "Pogresno napisana komanda\n");
+	}
+
 	return length;
 }
 
