fb5f5d0986faa85751eeeeb1a21bf03bb56fefe9
NemanjaKusic <nemanja.kusic99@gmail.com>
2021-12-14 12:07:51 +0100

    aplikacija case 1/4/5/7 gotov

--- Aplikacija/aplikacija.c
+++ Aplikacija/aplikacija.c
@@ -7,6 +7,7 @@ int main()
 	FILE* fp;
 	char* izbor;
 	size_t num_of_bytes = 1;
+
 	
 	while(1)
 	{
@@ -19,6 +20,7 @@ int main()
 		printf("6: Izbriši izraz iz stringa\n");
 		printf("7: Izbriši poslednjih n karaktera iz stringa\n");
 		printf("Q: Za izlaz iz aplikacije\n");
+		printf("Unesi broj ili Q:\n");
 
 		fp = fopen ("/dev/stred", "r");
 		if(fp==NULL) 
@@ -36,23 +38,96 @@ int main()
 			puts("Problem pri zatvaranju /dev/stred");
 			return -1;
 		}
+
+		char* string;
+		size_t num = 100;
 			
+		int br;
 
 		switch (*izbor)
 		{
 			case '1':
+				fp = fopen ("/dev/stred", "r");
+				if(fp==NULL)
+				{
+					puts("Problem pri otvaranju /dev/stred\n");
+					return -1;
+				}
+
+				string = (char *)malloc(num);		
+				getdelim(&string, &num, 0, fp);
+
+				if(fclose(fp))
+				{
+					puts("Problem pri zatvaranju /dev/stred\n");
+					return -1;
+				}
+			
+				printf("%s\n",string);	
+
+				free(string);
+
 				break;
 			case '2':
 				break;
 			case '3':
 				break;
 			case '4':
+				fp = fopen("/dev/stred", "w");
+				if(fp == NULL)
+				{
+					printf("Problem pri otvaranju /dev/stred\n");
+					return -1;
+				}
+
+				fputs("clear\n", fp);
+
+				if(fclose(fp))
+				{
+					printf("Problem pri zatvaranju /dev/stred\n");
+					return -1;
+				}
 				break;
 			case '5':
+				fp = fopen("/dev/stred", "w");
+				if(fp == NULL)
+				{
+					printf("Problem pri otvaranju /dev/stred\n");
+					return -1;
+				}
+
+				fputs("shrink\n", fp);
+
+				if(fclose(fp))
+				{
+					printf("Problem pri zatvaranju /dev/stred\n");
+					return -1;
+				}
 				break;
 			case '6':
 				break;
 			case '7':
+
+				fp = fopen("/dev/stred", "w");
+				if(fp == NULL)
+				{
+					printf("Problem pri otvaranju /dev/stred\n");
+					return -1;
+				}
+
+				printf("Broj karaktera za obrisati: ");
+				
+				scanf("%d", &br);
+		
+				fprintf(fp, "truncate=%d\n", br);
+
+				if(fclose(fp))
+				{
+					printf("Problem pri zatvaranju /dev/stred\n");
+					return -1;
+				}
+				
+
 				break;
 			case 'Q':
 				free(izbor);
